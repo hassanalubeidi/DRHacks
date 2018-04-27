6acc81aa29c042321c2b0dbb571b278184c9c6c8
ubuntu <you@example.com>
2018-04-27 20:44:41 +0100

    Parsing IDs, Title, Author

--- downloadParse.py
+++ downloadParse.py
@@ -1,9 +1,23 @@
 
-filepath = 'eprints-articles.json'  
-with open(filepath) as fp:  
+filepath = 'eprints-articles.json'
+with open(filepath) as fp:
    line = fp.readline()
    cnt = 1
    while line:
-       print("Line {}: {}".format(cnt, line.strip()))
+#ID_PARSE
+       if "eprintid" in line.strip():
+           line = line[15:-4]
+           print("ID is {}".format(line.strip()))
+
+#TITLE_PARSE
+       if "title" in line.strip():
+           line = line[14:-5]
+           print("Title is {}".format(line.strip()))
+
+#AUTHOR_PARSE
+       if "authors" in line.strip():
+           line = fp.readline()
+           line = line[7:-5]
+           print("AUTHOR is {}".format(line.strip()))
+
        line = fp.readline()
-       cnt += 1
