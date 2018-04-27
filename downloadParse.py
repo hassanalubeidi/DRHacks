e1eed71894a3be149c0216e73bb8e99a91c6ecf5
ubuntu <you@example.com>
2018-04-27 20:11:08 +0100

    Python file for downloading and parsing / JS file now purely for visuals

---
+++ downloadParse.py
@@ -0,0 +1,9 @@
+
+filepath = 'eprints-articles.json'  
+with open(filepath) as fp:  
+   line = fp.readline()
+   cnt = 1
+   while line:
+       print("Line {}: {}".format(cnt, line.strip()))
+       line = fp.readline()
+       cnt += 1
