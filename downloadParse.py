82f090ff7ff227f10ad62b8aa749adafc2a88d90
ubuntu <you@example.com>
2018-04-28 12:12:58 +0100

    Updated tmp

--- downloadParse.py
+++ downloadParse.py
@@ -15,14 +15,14 @@ with open(filepath) as fp:
 #ID_PARSE
        if "eprintid" in line.strip():
            line = line[15:-4]
-           print("ID is {}".format(line.strip()))
+
            with open("tmp.txt", "a") as myfile:
                myfile.write("!"+ line.strip() + "\n")
 
 #TITLE_PARSE
        if "title" in line.strip():
            line = line[14:-5]
-           print("Title is {}".format(line.strip()))
+
            with open("tmp.txt", "a") as myfile:
                myfile.write("@"+ line.strip() + "\n")
 
@@ -31,22 +31,21 @@ with open(filepath) as fp:
        if "\"type\"" in line.strip():
 #           (["'])(?:(?=(\\?))\2.)*?\1
            line = line[13:-5]
-           print("Type is {}".format(line.strip()))
            with open("tmp.txt", "a") as myfile:
                myfile.write("#"+ line.strip() + "\n")
 
 #jsonURI_PARSE
        if "json_uri" in line.strip():
            line = line[17:-3]
-           print("URI is {}".format(line.strip()))
+
            with open("tmp.txt", "a") as myfile:
-               myfile.write("$"+ line.strip() + "\n")
+               myfile.write("\n" + "$"+ line.strip() + "\n")
 
 #Get filesize
            d = urllib.urlopen(line)
            data = json.loads(d.read())
            fs = str(data['documents'][0]['files'][0]['filesize'])
-           print "File Size is: "  + fs
+
            with open("tmp.txt", "a") as myfile:
                myfile.write("%"+ fs  + "\n \n")
 
@@ -54,7 +53,7 @@ with open(filepath) as fp:
        if "authors" in line.strip():
            line = fp.readline()
            line = line[7:-5]
-           print("AUTHOR is {}".format(line.strip()))
+
            with open("tmp.txt", "a") as myfile:
                myfile.write("^"+ line.strip())
 
