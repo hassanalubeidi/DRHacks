7542ed1ad1b8a14952b7a2e6f6e9fcc2fc48a17b
ubuntu <you@example.com>
2018-04-28 12:02:19 +0100

    Download Parser

--- downloadParse.py
+++ downloadParse.py
@@ -1,58 +1,61 @@
+#!/usr/bin/env python
+
 import urllib, json
+import os
+import sys
 
 filepath = 'eprints-articles.json'
+
 open("tmp.txt", "w").close()
 
 with open(filepath) as fp:
-    line = fp.readline()
-    cnt = 1
+   line = fp.readline()
+ #! - id, @ - Title, # - Type, $ - URI, % - Size, ^ - Author
+   while line:
+#ID_PARSE
+       if "eprintid" in line.strip():
+           line = line[15:-4]
+           print("ID is {}".format(line.strip()))
+           with open("tmp.txt", "a") as myfile:
+               myfile.write("!"+ line.strip() + "\n")
 
-    while line:
- #ID_PARSE
-        if "eprintid" in line.strip():
-            line = line[15:-4]
-            print("ID is {}".format(line.strip()))
-            with open("tmp.txt", "a") as myfile:
-               myfile.write(line.strip() + "\n")
-
- #TITLE_PARSE
-        if "title" in line.strip():
-            line = line[14:-5]
-            print("Title is {}".format(line.strip()))
-            with open("tmp.txt", "a") as myfile:
-                myfile.write(line.strip() + "\n")
+#TITLE_PARSE
+       if "title" in line.strip():
+           line = line[14:-5]
+           print("Title is {}".format(line.strip()))
+           with open("tmp.txt", "a") as myfile:
+               myfile.write("@"+ line.strip() + "\n")
 
 #TYPE_PARSE
 #       if "'"'type'"':" in line.strip():
-        if "\"type\"" in line.strip():
+       if "\"type\"" in line.strip():
 #           (["'])(?:(?=(\\?))\2.)*?\1
-            line.strip() = line[13:-5]
-            print("Type is {}".format(line.strip()))
-            with open("tmp.txt", "a") as myfile:
-                myfile.write(line.strip() + "\n")
+           line = line[13:-5]
+           print("Type is {}".format(line.strip()))
+           with open("tmp.txt", "a") as myfile:
+               myfile.write("#"+ line.strip() + "\n")
 
- #jsonURI_PARSE
-        if "json_uri" in line.strip():
-            line = line[17:-3]
-            print("URI is {}".format(line.strip()))
-            with open("tmp.txt", "a") as myfile:
-                myfile.write("\n" + line + "\n")
+#jsonURI_PARSE
+       if "json_uri" in line.strip():
+           line = line[17:-3]
+           print("URI is {}".format(line.strip()))
+           with open("tmp.txt", "a") as myfile:
+               myfile.write("$"+ line.strip() + "\n")
 
- #Get filesize
-            d = urllib.urlopen(line)
-            data = json.loads(d.read())
-            print "File Size is: "  + str(data['documents'][0]['files'][0]['filesize'])
-            fs = str(data['documents'][0]['files'][0]['filesize'])
-            print "File Size is: "  + fs
-            with open("tmp.txt", "a") as myfile:
-                myfile.write(fs  + "\n \n")
+#Get filesize
+           d = urllib.urlopen(line)
+           data = json.loads(d.read())
+           fs = str(data['documents'][0]['files'][0]['filesize'])
+           print "File Size is: "  + fs
+           with open("tmp.txt", "a") as myfile:
+               myfile.write("%"+ fs  + "\n \n")
 
- #AUTHOR_PARSE
-        if "authors" in line.strip():
-            line = fp.readline()
-            line = line[7:-5]
-            print("AUTHOR is {}".format(line.strip()))
-            with open("tmp.txt", "a") as myfile:
-                myfile.write(line)
+#AUTHOR_PARSE
+       if "authors" in line.strip():
+           line = fp.readline()
+           line = line[7:-5]
+           print("AUTHOR is {}".format(line.strip()))
+           with open("tmp.txt", "a") as myfile:
+               myfile.write("^"+ line.strip())
 
-        line = fp.readline()
+       line = fp.readline()
